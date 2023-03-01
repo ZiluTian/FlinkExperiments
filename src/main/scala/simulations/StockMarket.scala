d0792b55b4b15b3690920d51feb488378ba31aa4
ZiluTian <zilu.tian@epfl.ch>
2023-03-01 22:42:18 +0100

    Fix the null pointer exception due to serialization error

--- src/main/scala/simulations/StockMarket.scala
+++ src/main/scala/simulations/StockMarket.scala
@@ -29,8 +29,8 @@ import org.apache.flink.types.NullValue
 import org.apache.flink.api.common.functions.MapFunction
 
 import scala.util.Random
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+// import org.slf4j.Logger
+// import org.slf4j.LoggerFactory
 import org.apache.flink.connector.file.src.FileSource
 import org.apache.flink.connector.file.src.reader.TextLineInputFormat
 import org.apache.flink.core.fs.Path
@@ -38,11 +38,13 @@ import org.apache.flink.api.common.io.FileInputFormat
 import org.apache.flink.api.java.io.TextInputFormat
 
 object StockMarket {
+
   def main(args: Array[String]): Unit = {
 	  // val LOG: Logger = LoggerFactory.getLogger("root")
 
     // set up the execution environment
     val env = ExecutionEnvironment.getExecutionEnvironment
+
     env.setParallelism(args(0).toInt)
     val edgeFilePath = args(1)
     
@@ -52,34 +54,47 @@ object StockMarket {
     // ===========Load edge file and create the graph =========
     // Graph<K, VV, EV>    
     // Update the path to the input edge file 
-    val edgeDataset: DataSet[Edge[LongValue, List[Double]]] = env.readTextFile(edgeFilePath).map(
-      new MapFunction[String, Edge[LongValue, List[Double]]]() { 
-        def map(value: String): Edge[LongValue, List[Double]] = { 
+    val edgeDataset: DataSet[Edge[LongValue, Double]] = env.readTextFile(edgeFilePath).map(
+      new MapFunction[String, Edge[LongValue, Double]]() { 
+        def map(value: String): Edge[LongValue, Double] = { 
           val fields = value.split(" ")
           val srcId: LongValue = new LongValue(fields(0).toLong)
           val targetId: LongValue = new LongValue(fields(1).toLong)
-          new Edge(srcId, targetId, List(0.0))
+          new Edge(srcId, targetId, 0.0)
         }
     })
 
-    val graph: Graph[LongValue, List[List[Double]], List[Double]] = Graph.fromDataSet(edgeDataset, env).mapVertices( 
-      new MapFunction[Vertex[LongValue, NullValue], List[List[Double]]]() { 
-        def map(value: Vertex[LongValue, NullValue]): List[List[Double]] =  {
-            val stock_timeseries = List(1000.0)
-            val marketState = List(0, 1000, 1000.0, 0, 0, 0, 0)
-            val timer = List(0.0)   // for calculating past average
-            val traderState = List(1000, 1, 1000.0)
-            val rules = List(0, 0, 0, 0, 0.0, Random.nextInt(5), 0)   // 5 rules and their respective strength, initially 0; most recent rule
-            List(stock_timeseries, marketState, timer, traderState, rules)
+    val graph: Graph[LongValue, Array[Array[Double]], Double] = Graph.fromDataSet(edgeDataset, env).mapVertices( 
+      new MapFunction[Vertex[LongValue, NullValue], Array[Array[Double]]]() { 
+        def map(value: Vertex[LongValue, NullValue]): Array[Array[Double]] =  {
+            val stock_timeseries: Array[Double] = Array(100.0)     
+            val lastDividend: Double = 0
+            val lastAvg: Double = 100.0
+            val currentPrice: Double = 100.0
+            val dividendIncrease: Double = 0
+            val recent10AvgInc: Double = 100.0
+            val recent50AvgInc: Double = 95.0  
+            val recent100AvgInc: Double = 107.1  
+            val marketState = Array(lastDividend, lastAvg, currentPrice, dividendIncrease, recent100AvgInc, recent50AvgInc, recent100AvgInc)
+
+            val timer = Array(0.0)   // for calculating past average
+            
+            val cash: Double = 1000.0
+            val shares: Double = 1
+            val estimatedWealth: Double = 1100.0
+            val traderState = Array(cash, shares, estimatedWealth)
+
+            val rules = Array(0, 0, 0, 0, 0.0, Random.nextInt(5), 0)   // 5 rules and their respective strength, initially 0; most recent rule
+            Array(stock_timeseries, marketState, timer, traderState, rules)
         }
     })
 
     // ===========Simulation-specific logic =========
 
-    val priceAdjustmentFactor = 0.01
-    val interestRate = 0.0001
+    val priceAdjustmentFactor: Double = 0.01
+    val interestRate: Double = 0.0001
 
-    def update(window: Double, timer: Double, lastAvg: Double, stock_timeseries: List[Double]): Int = {
+    def update(window: Double, timer: Double, lastAvg: Double, stock_timeseries: Array[Double]): Int = {
         // moving window    
         var calculated_avg: Double = -1
         var sumPrice: Double = 0
@@ -108,7 +123,8 @@ object StockMarket {
     }
 
     // return (action, cash, shares)
-    def evalRule(rule: Int, stockPrice: Double, marketState: List[Double], cash: Double, shares: Double): (Int, Double, Double) = {
+    def evalRule(rule: Int, stockPrice: Double, marketState: Array[Double], cash: Double, shares: Double): (Int, Double, Double) = {
+        // assert(marketState.size == 7)
         val lastDividend = marketState(0)
         val lastAvg = marketState(1)
         val currentPrice = marketState(2)
@@ -167,34 +183,41 @@ object StockMarket {
     }
 
     // K, V, E, M
-    final class StockMarketComputeFunction extends ComputeFunction[LongValue, List[List[Double]], List[Double], List[List[Double]]] {
+    final class StockMarketComputeFunction extends ComputeFunction[LongValue, Array[Array[Double]], Double, Array[Array[Double]]] {
 
-        override def compute(vertex: Vertex[LongValue, List[List[Double]]], messages: MessageIterator[List[List[Double]]]): Unit = {
-            val state: List[List[Double]] = vertex.getValue()
-            var stock_timeseries = state(0)
-            var marketState = state(1)
-            var lastDividend = marketState(0)
-            var lastAvg = marketState(1)
-            var currentPrice = marketState(2)
-            var dividendIncrease = marketState(3)
-            var recent10AvgInc = marketState(4)
-            var recent50AvgInc = marketState(5)    
-            var recent100AvgInc = marketState(6)       
-            var timer = state(2).head
-            var traderState = state(3)
-            var cash = traderState(0)
-            var shares = traderState(1)
-            var estimatedWealth = traderState(2)
+        override def compute(vertex: Vertex[LongValue, Array[Array[Double]]], messages: MessageIterator[Array[Array[Double]]]): Unit = {
+            val state = vertex.getValue().asInstanceOf[Array[Array[Double]]]
+            assert(state.size == 5)
+            // logger.info("State size is " + state.size)
+            var stock_timeseries: Array[Double] = state(0)
+            var marketState: Array[Double] = state(1)
+            assert(marketState.size == 7)
+            // logger.info("market state size is " + marketState.size)
+            var lastDividend: Double = marketState(0)
+            var lastAvg: Double = marketState(1)
+            var currentPrice: Double = marketState(2)
+            var dividendIncrease: Double = marketState(3)
+            var recent10AvgInc: Double = marketState(4)
+            var recent50AvgInc: Double = marketState(5)    
+            var recent100AvgInc: Double = marketState(6)       
+            var timer: Double = state(2).head
+            var traderState: Array[Double] = state(3)
+            assert(traderState.size == 3)
+            // logger.info("trader state size is " + traderState.size)
+            var cash: Double = traderState(0)
+            var shares: Double = traderState(1)
+            var estimatedWealth: Double = traderState(2)
 
-            var rules = state(4).toBuffer
+            val rules: Array[Double] = state(4)
             var lastRule: Int = rules(5).toInt
             var nextAction: Int = rules(6).toInt
+            assert(rules.size == 7)
 
             timer += 1 
-            if (vertex.getId() != 0) {  // trader 
+            if (vertex.getId().getValue != 0) {  // trader 
                 cash = cash * (1 + interestRate)
                 messages.forEach(m => {
-                    val ms: List[Double] = m.asInstanceOf[List[Double]]
+                    val ms = m.head
                     val m_dividendPerShare = ms(0)
                     val m_lastAvg = ms(1)
                     val m_currentPrice = ms(2)
@@ -214,7 +237,7 @@ object StockMarket {
                         rules(lastRule) -= 1
                     }
                     // Select the rule with the highest strength for the next action 
-                    val nextRule = rules.zipWithIndex.toList.sortBy(x => x._1).head._2
+                    val nextRule = rules.zipWithIndex.sortBy(x => x._1).head._2
                     // Obtain the action based on the rule 
                     val x = evalRule(nextRule, m_currentPrice, ms, cash, shares)
                     // Update lastRule with the recently selected rule 
@@ -223,19 +246,18 @@ object StockMarket {
                     rules(6) = x._1
                     cash = x._2
                     shares = x._3 
-
-                    val it = getEdges.iterator()
-                    while (it.hasNext) {
-                        val edge = it.next
-                        sendMessageTo(edge.getTarget, List(List(rules(6))))
-                    }
                 })
+                val it = getEdges.iterator()
+                while (it.hasNext) {
+                    val edge = it.next
+                    sendMessageTo(edge.getTarget, Array(Array(rules(6))))
+                }
             } else {    // market
                 var buyOrders: Int = 0
                 var sellOrders: Int = 0
 
                 messages.forEach(m => {
-                    val ms: List[Double] = m.asInstanceOf[List[Double]]
+                    val ms = m.head
                     if (ms(0)==1) {
                         buyOrders = buyOrders + 1
                     } else if (ms(0)==2) {
@@ -245,7 +267,7 @@ object StockMarket {
                 // Update price based on buy-sell orders
                 currentPrice = currentPrice*(1+priceAdjustmentFactor*(buyOrders - sellOrders))
                 // Update the stock time series with the new price
-                stock_timeseries = stock_timeseries ::: List(currentPrice)
+                stock_timeseries = stock_timeseries :+ currentPrice
                 // Increment the timer 
                 // Calculate the average of the stock price
                 lastAvg = stock_timeseries.reduce((a, b) => a + b) / timer 
@@ -271,37 +293,37 @@ object StockMarket {
                 val it = getEdges.iterator()
                 while (it.hasNext) {
                     val edge = it.next
-                    sendMessageTo(edge.getTarget, List(List(lastDividend, lastAvg, currentPrice, dividendIncrease, recent10AvgInc, recent50AvgInc, recent100AvgInc)))
+                    sendMessageTo(edge.getTarget, Array(Array(lastDividend, lastAvg, currentPrice, dividendIncrease, recent10AvgInc, recent50AvgInc, recent100AvgInc)))
                 }
             }
-            setNewVertexValue(List(stock_timeseries, List(lastDividend, lastAvg, currentPrice, dividendIncrease, recent10AvgInc, recent50AvgInc, recent100AvgInc), List(timer), 
-            List(cash, shares, estimatedWealth), rules.toList))
+            setNewVertexValue(Array(stock_timeseries, Array(lastDividend, lastAvg, currentPrice, dividendIncrease, recent10AvgInc, recent50AvgInc, recent100AvgInc), Array(timer), 
+            Array(cash, shares, estimatedWealth), rules))
       }
     }
 
     // message combiner
     // combinedMessage has the same type as Message
-    final class StockMarketCombiner extends MessageCombiner[LongValue, List[List[Double]]] {
+    final class StockMarketCombiner extends MessageCombiner[LongValue, Array[Array[Double]]] {
 
-        override def combineMessages(messages: MessageIterator[List[List[Double]]]): Unit = {
-            var combined: List[List[Double]] = List()
+        override def combineMessages(messages: MessageIterator[Array[Array[Double]]]): Unit = {
+            var combined: Array[Array[Double]] = Array() 
 
             while (messages.hasNext) {
               val msg = messages.next
-              combined = msg ::: combined
+              combined = combined ++ msg
             }
-            // LOG.info("Combined value is " + combined + " total messages " + totalMsgs)
             sendCombinedMessage(combined)
         }
     }
 
-    // // // Execute the vertex-centric iteration
+    // Execute the vertex-centric iteration
     val result = graph.runVertexCentricIteration(new StockMarketComputeFunction(), new StockMarketCombiner(), maxIterations)
 
-    // // // Extract the vertices as the result
-    val golResult = result.getVertices
+    // Extract the vertices as the result
+    val simulation = result.getVertices
 
-    // // execute
-    golResult.collect()
+    // execute
+    simulation.collect()
+    // simulation.print()
   }
 }
